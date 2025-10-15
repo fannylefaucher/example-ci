9f4443dba7cd43c790f28ec834a9b3236b6c8575
Fanny Lefaucher <fsama@dtu.dk>
2025-10-15 13:05:23 +0200

    added functions and tests

---
+++ functions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 15 12:54:08 2025
+
+@author: fsama
+"""
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b  # <--- fix this in step 7
+
+def multiply(a, b):
+    return a * b
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
